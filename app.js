118f82fb2ceb0406d078d1f67a640dd079ae03b6
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-22 14:18:50 +0200

    changed if statement + cleaned up code

--- app.js
+++ app.js
@@ -5,24 +5,18 @@ let poppedBalloons = 0
 balloonsEl.addEventListener("mouseover", event => {
   if (event.target.classList.contains("balloon")) {
     console.log("ballon")
-    event.target.classList.add("popped")
     event.target.querySelector("p").classList.remove("hide")
 
-    if (event.target.classList.contains("popped")) {
+    if (!event.target.classList.contains("popped")) {
       poppedBalloons++
-    } else {
-      return
     }
-    console.log(poppedBalloons)
-    if (poppedBalloons > 24) {
+    if (poppedBalloons === 24) {
       document.write("YAY!")
     }
-  } else {
-    //console.log("not ballon")
-    return
+    event.target.classList.add("popped")
+
+    console.log(poppedBalloons)
   }
 })
-//console.log(poppedBalloons)
 
 console.log(poppedBalloons)
-//document.body.appendChild(poppMsg)
