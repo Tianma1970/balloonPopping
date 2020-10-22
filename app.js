ea7ff9702b8247bed7b8b6c6569307390185d0a6
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-22 13:40:28 +0200

    added if statements for checking right className

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
-const popRed = document.querySelector(".balloonRed")
-const ploppTextRed = popRed.querySelector(".hide")
+const balloonsEl = document.querySelector(".balloons")
 
-const poppedBalloons = []
+let poppedBalloons = 0
 
-popRed.addEventListener("mouseenter", () => {
-  ploppTextRed.classList.remove("hide")
-  popRed.classList.add("popped")
-  poppedBalloons.push(popRed)
+balloonsEl.addEventListener("mouseover", event => {
+  if (event.target.classList.contains("balloon")) {
+    console.log("ballon")
+    event.target.classList.add("popped")
+    event.target.querySelector("p").classList.remove("hide")
+    poppedBalloons++
+    if (poppedBalloons > 4) {
+      document.write("YAY!")
+    }
+  } else {
+    console.log("not ballon")
+  }
 })
 //console.log(poppedBalloons)
 
-//Plopp the blue balloon
-const popBlue = document.querySelector(".balloonBlue")
-const ploppTextBlue = popBlue.querySelector(".hideBlue")
-
-popBlue.addEventListener("mouseenter", () => {
-  ploppTextBlue.classList.remove("hideBlue")
-  popBlue.classList.add("popped")
-  poppedBalloons.push(popBlue)
-})
-
-//Plopp the Yellow balloon
-const popYellow = document.querySelector(".balloonYellow")
-const ploppTextYellow = popYellow.querySelector(".hideYellow")
-
-popYellow.addEventListener("mouseenter", () => {
-  ploppTextYellow.classList.remove("hideYellow")
-  popYellow.classList.add("popped")
-  poppedBalloons.push(popYellow)
-})
-
 console.log(poppedBalloons)
-if ((poppedBalloons.length = 3)) {
-  let poppMsg = document.createElement("SPAN")
-  document.body.appendChild(poppMsg)
-  //alert("YAY!")
-}
+//document.body.appendChild(poppMsg)
