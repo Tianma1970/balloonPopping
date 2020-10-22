8facd75b132bcf9ee1ed85de711d6ae9ec14ae53
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-22 11:42:53 +0200

    added blue and yellow balloons to be popped

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const popRed = document.querySelector("section")
-const ploppText = popRed.querySelector("p")
+const popRed = document.querySelector(".balloonRed")
+const ploppTextRed = popRed.querySelector(".hide")
 
 popRed.addEventListener("mouseenter", () => {
-  ploppText.classList.remove("hide")
+  ploppTextRed.classList.remove("hide")
 
   popRed.classList.add("popped")
   console.log("test")
+})
+//Plopp the blue balloon
+const popBlue = document.querySelector(".balloonBlue")
+const ploppTextBlue = popBlue.querySelector(".hideBlue")
+
+popBlue.addEventListener("mouseenter", () => {
+  ploppTextBlue.classList.remove("hideBlue")
+  popBlue.classList.add("popped")
+})
+
+//Plopp the Yellow balloon
+const popYellow = document.querySelector(".balloonYellow")
+const ploppTextYellow = popYellow.querySelector(".hideYellow")
+
+popYellow.addEventListener("mouseenter", () => {
+  ploppTextYellow.classList.remove("hideYellow")
+  popYellow.classList.add("popped")
 })
